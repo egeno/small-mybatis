e080bed7fe5e4d3cbf7557797add6be457dcca32
付政委 <184172133@qq.com>
2022-06-01 13:25:34 +0800

    小傅哥，feat：测试案例

--- mybatis-step-00/src/test/java/cn/bugstack/mybatis/test/ApiTest.java
+++ mybatis-step-00/src/test/java/cn/bugstack/mybatis/test/ApiTest.java
@@ -13,6 +13,10 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  * @author 小傅哥，微信：fustack
@@ -40,6 +44,40 @@ public class ApiTest {
         // 4. 测试验证
         User user = userDao.queryUserInfo(new User(1L));
         logger.info("测试结果：{}", JSON.toJSONString(user));
+    }
+
+    @Test
+    public void test_jdbc() throws Exception {
+        // 1.加载驱动
+        Class.forName("com.mysql.jdbc.Driver");
+
+        // 2.连接信息
+        String url = "jdbc:mysql://127.0.0.1:3306/mybatis?useUnicode=true&characterEncoding=utf8&useSSL=true";
+        String username = "root";
+        String password = "123456";
+
+        // 3.连接成功
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+        // 4.执行SQL的对象获取
+        Statement statement = connection.createStatement();
+
+        // 5.待执行SQL
+        String sql = "SELECT id, userId, userName, userHead FROM user";
+
+        // 6.执行SQL，并输出结果
+        ResultSet resultSet = statement.executeQuery(sql);
+        while (resultSet.next()) {
+            System.out.print("id=" + resultSet.getObject("id") + " ");
+            System.out.print("userId=" + resultSet.getObject("userId") + " ");
+            System.out.print("userName=" + resultSet.getObject("userName") + " ");
+            System.out.print("userHead=" + resultSet.getObject("userHead"));
+        }
+
+        // 7.释放连接
+        resultSet.close();
+        statement.close();
+        connection.close();
     }
 
 }
