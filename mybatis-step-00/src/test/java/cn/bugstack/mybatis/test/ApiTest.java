3ab64888705ba0116c8f75157ffecdd244dd49fe
付政委 <184172133@qq.com>
2022-07-04 21:33:42 +0800

    小傅哥，feat：二级缓存案例

--- mybatis-step-00/src/test/java/cn/bugstack/mybatis/test/ApiTest.java
+++ mybatis-step-00/src/test/java/cn/bugstack/mybatis/test/ApiTest.java
@@ -30,22 +30,30 @@ public class ApiTest {
 
     @Test
     public void test_SqlSessionFactory() throws IOException {
+
         // 1. 从SqlSessionFactory中获取SqlSession
         Reader reader = Resources.getResourceAsReader("mybatis-config-datasource.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
 
-        // 2. 开启 Session
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-
-        // 3. 获取映射器对象
-        IActivityDao dao = sqlSession.getMapper(IActivityDao.class);
-
+        // 2. 请求对象
         Activity req = new Activity();
         req.setActivityId(100001L);
-        Activity res = dao.queryActivityById(req);
 
-        logger.info("测试结果：{}", JSON.toJSONString(res));
+        // 3. 第一组：SqlSession
+        // 3.1 开启 Session
+        SqlSession sqlSession01 = sqlSessionFactory.openSession();
+        // 3.2 获取映射器对象
+        IActivityDao dao01 = sqlSession01.getMapper(IActivityDao.class);
+        logger.info("测试结果01：{}", JSON.toJSONString(dao01.queryActivityById(req)));
+        sqlSession01.close();
 
+        // 4. 第一组：SqlSession
+        // 4.1 开启 Session
+        SqlSession sqlSession02 = sqlSessionFactory.openSession();
+        // 4.2 获取映射器对象
+        IActivityDao dao02 = sqlSession02.getMapper(IActivityDao.class);
+        logger.info("测试结果02：{}", JSON.toJSONString(dao02.queryActivityById(req)));
+        sqlSession02.close();
     }
 
     @Test
